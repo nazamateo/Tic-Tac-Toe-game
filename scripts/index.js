35435dfcc946cfd76570e1cabcbc9f3a9f12e74e
naza mateo <nazamateo@gmail.com>
2022-03-02 21:17:26 +0800

    nth commit almost there

--- scripts/index.js
+++ scripts/index.js
@@ -1,62 +1,54 @@
 var moves = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
 ]
 
 var moveshistory = []
-
-var m1 = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-var m2 = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-var m3 = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-var m4 = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-var m5 = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-var m6 = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-var m7 = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-var m8 = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-var m9 = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
 
 var move
 var movecounter = 1
 var score1 = 0 //O
 var score2 = 0 //X
 var scoretie = 0 //tie
+var prevvv = document.getElementById("previous")
+console.log(prevvv)
+var nexxx = document.getElementById("next")
+console.log(nexxx)
+
+var a = document.getElementById("i0j0")
+var b = document.getElementById("i0j1")
+var c = document.getElementById("i0j2")
+var d = document.getElementById("i1j0")
+var e = document.getElementById("i1j1")
+var f = document.getElementById("i1j2")
+var g = document.getElementById("i2j0")
+var h = document.getElementById("i2j1")
+var i = document.getElementById("i2j2")
+
+a.onclick=gamecell()
+b.onclick=gamecell()
+c.onclick=gamecell()
+d.onclick=gamecell()
+e.onclick=gamecell()
+f.onclick=gamecell()
+g.onclick=gamecell()
+h.onclick=gamecell()
+i.onclick=gamecell()
+
+function displayH() {
+    prevvv.style.display = "block"
+    nexxx.style.display = "block"
+    console.log("displayme")
+}
+
+function hideH(){
+    prevvv.style.display = "none"
+    nexxx.style.display = "none"
+    console.log("imhidden")
+}
+
+
 
 //adds points to O
 function winnerO() {
@@ -65,6 +57,9 @@ function winnerO() {
     document.getElementById("score1").innerHTML = score1
     document.getElementById("score2").innerHTML = score2
     document.getElementById("scoretie").innerHTML = scoretie
+
+    //display prev and next function
+    displayH()
 }
 
 //adds points to X
@@ -74,6 +69,9 @@ function winnerX() {
     document.getElementById("score1").innerHTML = score1
     document.getElementById("score2").innerHTML = score2
     document.getElementById("scoretie").innerHTML = scoretie
+
+    //display prev and next function
+    displayH()
 }
 
 //identifies gamewinner
@@ -145,6 +143,8 @@ function gameWinner() {
         document.getElementById("scoretie").innerHTML = scoretie
         document.getElementById("score1").innerHTML = score1
         document.getElementById("score2").innerHTML = score2
+        //display prev and next function
+        displayH()
     }
 }
 
@@ -157,7 +157,7 @@ function gamecell() {
 
         //identifies who moves first
         if ((clicked.innerHTML.length) > 0) {
-            alert("this cell is occupied")
+            alert("Invalid move")
         } else {
             if (document.getElementById("one").checked) {
                 if ((movecounter % 2) === 0) {
@@ -188,15 +188,14 @@ function gamecell() {
                     if (i === cellidinum && j === cellidjnum) {
                         moves[i][j] = move
                         console.log(moves[i][j])
+                        console.log(moves)
+
+                        ///save current gameboard into array
+                        moveshistory.push(JSON.parse(JSON.stringify(moves)))
+                        console.log(moveshistory)
                     }
                 }
             }
-
-            ///save current gameboard into array
-
-
-
-
             //identify winner
             gameWinner()
         }
@@ -211,15 +210,16 @@ function gameReset() {
         ["", "", ""]
     ]
     movecounter = 1
-    document.getElementById("i0j0").textContent = ""
-    document.getElementById("i0j1").textContent = ""
-    document.getElementById("i0j2").textContent = ""
-    document.getElementById("i1j0").textContent = ""
-    document.getElementById("i1j1").textContent = ""
-    document.getElementById("i1j2").textContent = ""
-    document.getElementById("i2j0").textContent = ""
-    document.getElementById("i2j1").textContent = ""
-    document.getElementById("i2j2").textContent = ""
+    a.textContent = ""
+    b.textContent = ""
+    c.textContent = ""
+    d.textContent = ""
+    e.textContent = ""
+    f.textContent = ""
+    g.textContent = ""
+    h.textContent = ""
+    i.textContent = ""
+    hideH()
 }
 
 function clearScore() {
@@ -231,120 +231,18 @@ function clearScore() {
     document.getElementById("score2").innerHTML = score2
 }
 
+var displayArray = (moveshistory.length) - 1
+hcounter = JSON.parse(JSON.stringify(displayArray))
+console.log(typeof hcounter)
+console.log(hcounter)
 
 
-/**if(movecounter=2){
-                            m1[0][0] = document.getElementById("i0j0").textContent
-                            m1[0][1] = document.getElementById("i0j1").textContent
-                            m1[0][2] = document.getElementById("i0j2").textContent 
-                            m1[1][0] = document.getElementById("i1j0").textContent
-                            m1[1][1] = document.getElementById("i1j1").textContent
-                            m1[1][2] = document.getElementById("i1j2").textContent
-                            m1[2][0] = document.getElementById("i2j0").textContent
-                            m1[2][1] = document.getElementById("i2j1").textContent
-                            m1[2][2] = document.getElementById("i2j2").textContent
-                            console.log(`m1: ${m1}`)
-                            }
-                            
-                            if(movecounter=3){
-                            m2[0][0] = document.getElementById("i0j0").textContent
-                            m2[0][1] = document.getElementById("i0j1").textContent
-                            m2[0][2] = document.getElementById("i0j2").textContent 
-                            m2[1][0] = document.getElementById("i1j0").textContent
-                            m2[1][1] = document.getElementById("i1j1").textContent
-                            m2[1][2] = document.getElementById("i1j2").textContent
-                            m2[2][0] = document.getElementById("i2j0").textContent
-                            m2[2][1] = document.getElementById("i2j1").textContent
-                            m2[2][2] = document.getElementById("i2j2").textContent
-                            }
-                            
-                            if(movecounter=4){
-                            m3[0][0] = document.getElementById("i0j0").textContent
-                            m3[0][1] = document.getElementById("i0j1").textContent
-                            m3[0][2] = document.getElementById("i0j2").textContent 
-                            m3[1][0] = document.getElementById("i1j0").textContent
-                            m3[1][1] = document.getElementById("i1j1").textContent
-                            m3[1][2] = document.getElementById("i1j2").textContent
-                            m3[2][0] = document.getElementById("i2j0").textContent
-                            m3[2][1] = document.getElementById("i2j1").textContent
-                            m3[2][2] = document.getElementById("i2j2").textContent
-                            }
-                            
-                            
-                            
-                            if(movecounter=5){
-                            m4[0][0] = document.getElementById("i0j0").textContent
-                            m4[0][1] = document.getElementById("i0j1").textContent
-                            m4[0][2] = document.getElementById("i0j2").textContent 
-                            m4[1][0] = document.getElementById("i1j0").textContent
-                            m4[1][1] = document.getElementById("i1j1").textContent
-                            m4[1][2] = document.getElementById("i1j2").textContent
-                            m4[2][0] = document.getElementById("i2j0").textContent
-                            m4[2][1] = document.getElementById("i2j1").textContent
-                            m4[2][2] = document.getElementById("i2j2").textContent
-                            }
-                            
-                            if(movecounter=6){
-                            m5[0][0] = document.getElementById("i0j0").textContent
-                            m5[0][1] = document.getElementById("i0j1").textContent
-                            m5[0][2] = document.getElementById("i0j2").textContent 
-                            m5[1][0] = document.getElementById("i1j0").textContent
-                            m5[1][1] = document.getElementById("i1j1").textContent
-                            m5[1][2] = document.getElementById("i1j2").textContent
-                            m5[2][0] = document.getElementById("i2j0").textContent
-                            m5[2][1] = document.getElementById("i2j1").textContent
-                            m5[2][2] = document.getElementById("i2j2").textContent
-                            }
-                            
-                            
-                            if(movecounter=7){
-                            m6[0][0] = document.getElementById("i0j0").textContent
-                            m6[0][1] = document.getElementById("i0j1").textContent
-                            m6[0][2] = document.getElementById("i0j2").textContent 
-                            m6[1][0] = document.getElementById("i1j0").textContent
-                            m6[1][1] = document.getElementById("i1j1").textContent
-                            m6[1][2] = document.getElementById("i1j2").textContent
-                            m6[2][0] = document.getElementById("i2j0").textContent
-                            m6[2][1] = document.getElementById("i2j1").textContent
-                            m6[2][2] = document.getElementById("i2j2").textContent
-                            }
-                            
-                            
-                            
-                            if(movecounter=8){
-                            m7[0][0] = document.getElementById("i0j0").textContent
-                            m7[0][1] = document.getElementById("i0j1").textContent
-                            m7[0][2] = document.getElementById("i0j2").textContent 
-                            m7[1][0] = document.getElementById("i1j0").textContent
-                            m7[1][1] = document.getElementById("i1j1").textContent
-                            m7[1][2] = document.getElementById("i1j2").textContent
-                            m7[2][0] = document.getElementById("i2j0").textContent
-                            m7[2][1] = document.getElementById("i2j1").textContent
-                            m7[2][2] = document.getElementById("i2j2").textContent
-                            }
-                            
-                            if(movecounter=9){
-                            m8[0][0] = document.getElementById("i0j0").textContent
-                            m8[0][1] = document.getElementById("i0j1").textContent
-                            m8[0][2] = document.getElementById("i0j2").textContent 
-                            m8[1][0] = document.getElementById("i1j0").textContent
-                            m8[1][1] = document.getElementById("i1j1").textContent
-                            m8[1][2] = document.getElementById("i1j2").textContent
-                            m8[2][0] = document.getElementById("i2j0").textContent
-                            m8[2][1] = document.getElementById("i2j1").textContent
-                            m8[2][2] = document.getElementById("i2j2").textContent
-                            }
-                            
-                            
-                            
-                            if(movecounter=10){
-                            m9[0][0] = document.getElementById("i0j0").textContent
-                            m9[0][1] = document.getElementById("i0j1").textContent
-                            m9[0][2] = document.getElementById("i0j2").textContent 
-                            m9[1][0] = document.getElementById("i1j0").textContent
-                            m9[1][1] = document.getElementById("i1j1").textContent
-                            m9[1][2] = document.getElementById("i1j2").textContent
-                            m9[2][0] = document.getElementById("i2j0").textContent
-                            m9[2][1] = document.getElementById("i2j1").textContent
-                            m9[2][2] = document.getElementById("i2j2").textContent
-                            }*/
+function prev() {
+    hcounter--
+    console.log(`this will be displayed ${moveshistory[hcounter]}`)
+}
+
+function nexx() {
+    hcounter++
+    console.log(`this will be displayed ${moveshistory[hcounter]}`)
+}
